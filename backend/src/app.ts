d2fe1e7d9957b74e629b21ef44e0d504f6840f3f
Samuel Meijer <samuel.j.meijer@gmail.com>
2026-05-27 11:58:26 +0200

    Adding basic functionality for adding a task to db and fetching all tasks from db

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,27 @@
 import express, { Request, Response } from "express";
+import * as db from "./database";
 
 export const app = express();
 app.use(express.json());
 
+// TODO: Fixa rätt med routes
+// TODO: Fixa validering från requests innan kommunkation med DB
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "GET '/' - Hello World!" });
+});
+
+app.get("/all", (req: Request, res: Response) => {
+  const tasks = db.getAllTasks();
+
+  res.json(tasks);
+});
+
+// Testar för att se så jag kan lägga till en task - Fungerar!
+// TODO: Fixa rätt med route
+app.post("/", (req: Request, res, Response) => {
+  // TODO: Adderar validation
+
+  const newTask = db.createTask(req.body);
+  // TODO: Adderar felhantering
+  res.json({ message: `POST '/' - Adding task with id: ${newTask.id}` });
 });
